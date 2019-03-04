357fb4d51e84965823a07d98ce26ab2ef7d908f6
Thea Leake <thea.leake@gmail.com>
2019-03-03 16:36:40 -0800

    Added conditional for toggling debug msgs

--- src/defn_debug/core.clj
+++ src/defn_debug/core.clj
@@ -1,19 +1,31 @@
 (ns defn-debug.core
   (:require [clojure.tools.logging :as log]))
 
+(defn print-if-debug [message]
+  "Prints debug message if defn-debug-enabled is defined and truthy.
+  does nothing if defn-debug-enabled is not defined."
+  (let [is_enabled? (resolve 'defn-debug-enabled)]
+    (if is_enabled?
+      (if (var-get is_enabled?)
+        (println message)))))
+
 (defn print-invoke [name args env]
-  (println {:fn-name name
-   :fn-args args
-   :fn-env env
-   :exec-point "invocation"}))
+  "Prints out map with fn name, args, and env at time of defn.
+  Should be called at time of fn invocation."
+  (print-if-debug {:fn-name name
+                   :fn-args args
+                   :defn-env env
+                   :exec-point "invocation"}))
 
 (defn print-return [name result]
-  (println {:fn-name name
-            :fn-return result
-            :exec-point "return"}))
-
+  "Prints out map with fn name, and return value.
+  Should be called when fn returns."
+  (print-if-debug {:fn-name name
+                   :fn-return result
+                   :exec-point "return"}))
 
 (defmacro defnp [fn-name fn-args  & forms]
+  "Creates a fn with debug logging of fn args, and fn return val"
   `(defn ~fn-name ~fn-args
      (do
        (print-invoke `~~fn-name ~fn-args ~&env)
